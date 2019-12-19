9519f062a38261b716d04619659e90ca64a8eca0
Kopec <konrad.kopec@delphi.com>
2019-12-19 12:04:09 +0100

    files to compare preapred

--- src/main/java/mainControl/Main.java
+++ src/main/java/mainControl/Main.java
@@ -7,12 +7,11 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws Exception {
         DateNow.getCurrentDateTimeStartString();
+
         new MainController().run();
 
-        for (int i = 0; i < 10; i++) {
-            TimeUnit.MILLISECONDS.sleep(200);
-            System.out.print(".");
-        }
+        //TimeUnit.SECONDS.sleep(1);
+
         DateNow.getCurrentDateTimeEndString();
     }
 }
