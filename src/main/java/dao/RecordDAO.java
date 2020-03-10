a54d53ed8d18ef2a12b2f0afc1a15631757f16a0
Kopec <konrad.kopec@aptiv.com>
2020-03-10 16:48:23 +0100

    nothing

--- src/main/java/dao/RecordDAO.java
+++ src/main/java/dao/RecordDAO.java
@@ -7,18 +7,13 @@ import java.sql.Statement;
 
 public class RecordDAO {
 
+}
+    public static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=dbName";
+    public static final String USERNAME = "xxxx";
+    public static final String PASSWORD = "xxxx";
+
     public void showRecords() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/demo", "user1", "user123!");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("use testDb;SELECT * FROM  dbo.[11_CPD-LAH_Software_test];");
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+
+
     }
 }
